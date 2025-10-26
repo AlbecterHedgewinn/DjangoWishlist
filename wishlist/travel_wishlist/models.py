851aad953e87db9bdadd647eb472a2a94c036ea6
danielpbittner <fakeemail@gmail.com>
2025-10-26 18:49:13 -0500

    added Place model

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Create a Place model
+class Place(models.Model):
+    name = models.CharField(max_length=200)
+    visited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name}, visited? {self.visited}'
+        # this will show the name of the place and whether or not it has been visited
+        # visited? True or False
