029174faf7d367b1241a63d2d87cfaa8395f7be4
danielpbittner <fakeemail@gmail.com>
2025-10-26 19:45:31 -0500

    edited views.py to reflect the html format

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 
 # Create a view to show the list of places
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.all()
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
