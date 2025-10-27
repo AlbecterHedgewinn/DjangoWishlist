ac9450e4c4908db544507780a5eb9443f413a06a
danielpbittner <fakeemail@gmail.com>
2025-10-26 19:58:52 -0500

    more edits to views.py to handle POST and NOT POST requests

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Place
 from .forms import NewPlaceForm
 
@@ -7,6 +7,20 @@ from .forms import NewPlaceForm
 # Create a view to show the list of places
 # filter only unvisited places and order them by name
 def place_list(request):
+
+    # Handle the form submission
+    # if the request method is POST, we need to process the form data
+    # create a NewPlaceForm instance with the POST data
+    # if the form is valid, save the new place to the database
+    # then redirect to the place_list view to show the updated list
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            form.save()
+            return redirect('place_list')
+
+    # if not a POST request, just show the list of unvisited places
     places = Place.objects.filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places})
