4b1aac4fbaa11181d5eec2bbad1e5a15c12d1cad
danielpbittner <fakeemail@gmail.com>
2025-10-26 19:55:27 -0500

    added NewPlaceForm import and useage in views.py

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Place
+from .forms import NewPlaceForm
 
 # Create your views here.
 
@@ -7,4 +8,5 @@ from .models import Place
 # filter only unvisited places and order them by name
 def place_list(request):
     places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places})
