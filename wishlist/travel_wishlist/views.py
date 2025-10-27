b445501c23f0dcf3ce97765c32c9ea37e77862de
danielpbittner <fakeemail@gmail.com>
2025-10-26 19:35:46 -0500

    added place_list view to wishlist/travel_wishlist/views.py

--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Create a view to show the list of places
+def place_list(request):
+    return render(request, 'travel_wishlist/wishlist.html')
