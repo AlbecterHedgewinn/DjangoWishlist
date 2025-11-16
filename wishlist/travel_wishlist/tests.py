ef259924584c23684761402068687ec9565468ab
danielpbittner <fakeemail@gmail.com>
2025-11-16 14:30:56 -0600

    added even more tests

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -53,3 +53,52 @@ class TestVisitedPlaces(TestCase):
         self.assertContains(response, 'San Francisco')
         self.assertNotContains(response, 'Tokyo')
         self.assertNotContains(response, 'New York')
+
+# Test for adding a new place
+class TestAddNewPlace(TestCase):
+
+    def test_add_new_invisited_place_to_wishlist(self):
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'Tokyo', 'visited': False}
+
+        response = self.client.post(add_place_url, new_place_data, follow=True)
+        # Check that the correct template was used and the new place appears on the wishlist page
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        response_places = response.context['places']
+        #should be 1 place in the response context
+        self.assertEqual(1, len(response_places))
+        tokyo_response = response_places[0]
+
+        #expect this data to be in the database. Use get() to retrieve it
+        tokyo_in_db = Place.objects.get(name='Tokyo', visited=False)
+
+        # is data in the response the same as data in the database?
+        self.assertEqual(tokyo_in_db, tokyo_response)
+
+# test for visiting a place
+class TestVisitPlace(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_visit_place(self):
+
+        # visist place pk 2 (New York)
+        visit_place_url = reverse('place_was_visited', args=(2,)) # New York pk is 2 in the fixture
+        response = self.client.post(visit_place_url, follow=True) # follow redirects
+
+        # check correct template used
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # check New York is no longer in the wishlist
+        self.assertNotContains(response, 'New York')
+
+        # is new york visited in the database?
+        new_york = Place.objects.get(pk=2)
+        self.assertTrue(new_york.visited)
+
+    # Test visist nonexistent place
+    def test_visit_nonexistent_place(self):
+        visit_place_url = reverse('place_was_visited', args=(200,)) # non-existent pk
+        response = self.client.post(visit_place_url, follow=True)
+        self.assertEqual(404, response.status_code) # expect 404 not found
