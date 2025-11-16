a01224332afaff7374861b1ac7e219414fea1a23
danielpbittner <fakeemail@gmail.com>
2025-11-16 12:33:42 -0600

    added models to model.py for photo, date visited, user, and notes

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 # Create a Place model
 class Place(models.Model):
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)    # link to the User model, cascade means if the user is deleted, delete their places too
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null=True)
+    date_visited = models.DateField(null=True, blank=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}, visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else "No Photo"
+        return f'{self.name}, visited? {self.visited} on {self.date_visited}. Photo: {photo_str}'
         # this will show the name of the place and whether or not it has been visited
         # visited? True or False
+        # it will also show the date visited and the photo URL if available
