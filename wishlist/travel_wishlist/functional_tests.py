a22312ec32344a5c508f83c7c3239f783015c761
danielpbittner <fakeemail@gmail.com>
2025-11-16 16:00:54 -0600

    added test_add_new_places functional test

--- wishlist/travel_wishlist/functional_tests.py
+++ wishlist/travel_wishlist/functional_tests.py
@@ -21,5 +21,18 @@ class TitleTest (LiveServerTestCase):
         self.selenium.get(self.live_server_url)
         self.assertIn('Travel Wishlist', self.selenium.title)
 
+def test_add_new_place(self):
+    self.selenium.get(self.live_server_url)
+    input_name = self.selenium.find_element_by_id('id_name') 
+    input_name.send_keys('Denver')
+
+    add_button = self.selenium.find_element_by_id('add-new-place') 
+    add_button.click()
+
+    denver = self.selenium.find_element_by_id('place-name-5') 
+    self.assertEqual('Denver', denver.text)
+    self.assertIn('Denver', self.selenium.page_source) 
+    self.assertIn ('New York', self.selenium.page_source) 
+    self.assertIn('Tokyo', self.selenium.page_source)
 
 # To run: # python manage.py test wishlist.functional_tests.TitleTest
