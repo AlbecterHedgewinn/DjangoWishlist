97096e7ab6676f9147af06766e789fe3debe5a04
danielpbittner <fakeemail@gmail.com>
2025-11-16 17:08:53 -0600

    added methods in models.py to handle photo deletion and saving

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
+# import default_storage to handle file deletions
 
 # Create your models here.
 
@@ -11,6 +13,20 @@ class Place(models.Model):
     notes = models.TextField(blank=True, null=True)
     date_visited = models.DateField(null=True, blank=True)
     photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
+
+    # Override the save method to handle photo replacement
+    def save(self, *args, **kwargs):
+        old_place = Place.objects.filter(pk=self.pk).first()
+        if old_place and old_place.photo:
+            if old_place.photo != self.photo:
+                self.delete_photo(old_place.photo)
+        super().save(*args, **kwargs)
+        # super calls the original django save method to save the instance
+
+    # Method to delete photo from storage
+    def delete_photo(self, photo):
+        if default_storage.exists(photo.name):
+            default_storage.delete(photo.name)
 
     def __str__(self):
         photo_str = self.photo.url if self.photo else "No Photo"
